f307d8e56fa73559be6250a22643a08977fe8c7f
heinrichmeyer08 <heinrichmeyer08@gmail.com>
2023-01-21 23:59:58 +0100

    add Google auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,15 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "sportal24dd3cce": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "HostedUIDomain": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "OAuthMetadata": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
---
+++ src/Pages/LoginPage.js
@@ -0,0 +1,19 @@
+import { Auth } from 'aws-amplify'
+
+function LoginPage() {
+  return (
+    <div>
+    {/*
+      <h1>Login</h1>
+      <button onClick={() => Auth.federatedSignIn({ provider: 'Facebook' })}>
+        Sign In with Facebook
+      </button>
+  */}
+      <button onClick={() => Auth.federatedSignIn({ provider: 'Google' })}>
+        Sign In with Google
+      </button>
+    </div>
+  )
+}
+
+export default LoginPage
